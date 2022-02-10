a2fb80a16070a2e748ac92cc4226124eba753f83
Jsarabia-dev <jsarabia.dev@gmail.com>
2022-02-10 17:24:25 -0300

    Version 1.0.0-rc ready

--- test/data/products.ts
+++ test/data/products.ts
@@ -3,11 +3,11 @@ import { Product } from "../../dist/interfaces/interfaces";
 export const products: Product[] = [
   {
     id: '1',
-    title: 'Coffee Mug - Without image',
+    title: 'Coffee Mug! - Without image',
   },
   {
     id: '2',
     img: './coffee-mug2.png',
-    title: 'Coffee Mug - With image',
+    title: 'Coffee Mug! - With image',
   },
 ];
